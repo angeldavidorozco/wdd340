3922a9f5e5086b14e40a5274b2886fa9a27ade14
Angel Orozco <angeldorozcon@gmail.com>
2024-11-10 16:24:48 -0300

    Task: Finish page

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const expressLayouts = require("express-ejs-layouts")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+app.use(express.static('public'));
 
 /* ***********************
  * Routes
